08170849085b07e1bba4bb32c405a1418ee98e0f
amish <pithvaamish76@gmail.com>
2026-03-31 03:20:59 +0530

    User service auth and Entity Init

---
+++ user-service/src/main/java/com/example/user_service/entity/User.java
@@ -0,0 +1,61 @@
+package com.example.user_service.entity;
+
+import com.example.user_service.constant.Role;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Column(nullable = true, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    // Optional: for profile
+    private String profileImageUrl;
+
+    // Optional: location (for delivery use-case later)
+    private String address;
+
+    private String city;
+
+    // Audit fields
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime lastLogin;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
